fd30d5591a565343b177d137201209b4c3e4eb44
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:09:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import numpy as np
 st.title("Trabajo Práctico Modulo 01")
-st.sidebar.title("Parámetros")
-
 st.image("Python_logo.png")
-st.sidebar.image("DMC.png")
+st.image("DMC.png")
+st.subtheader("Oscar David Penaherrera Cordova)
+
+
+
+
 
 
