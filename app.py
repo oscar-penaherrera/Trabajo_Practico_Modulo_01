dadf74e6fcf120c9aedc9f116cd99cee8ae577f4
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:10:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 st.title("Trabajo Práctico Modulo 01")
 st.image("Python_logo.png")
 st.image("DMC.png")
-st.subtheader("Oscar David Penaherrera Cordova")
+st.subheader("Oscar David Penaherrera Cordova")
 
 
 
