092b4abf40d019b2350483628072948672b21622
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:26:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import numpy as np
 col_img1, col_img2 = st.columns(2) # Esto crea dos columnas de igual ancho
 
 with col_img1:
-    st.image("Python_logo.png", caption="Python Logo (Columna 1)", width=150)
+    st.image("Python_logo.png", , width=150)
 
 with col_img2:
-    st.image("DMC.png", caption="DMC Logo (Columna 2)", width=150)
+    st.image("DMC.png", , width=150)
 
 st.write()
     
