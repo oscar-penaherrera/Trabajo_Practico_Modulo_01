2f4479decbbf04e3f43f0849c5ad6898a9c35ecd
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:24:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import numpy as np
 
-st.image("Python_logo.png")
 col_left, col_rest = st.columns([1, 3]) # Columna pequeña a la izquierda, el resto del espacio a la derecha
 with col_left:
     st.image("Python_logo.png",)
 
-st.image("DMC.png")
+col_rest_right, col_right = st.columns([3, 1]) # El espacio restante a la izquierda, columna pequeña a la derecha
+with col_right:
+    st.image("DMC.png")
+    
 st.title("Trabajo Práctico Modulo 01")
 st.subheader("Oscar David Penaherrera Cordova")
 
