b1a708d5e692aba9b8d7d3bc558fb86cc7627792
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 14:04:31 -0500

    Change run_path to execfile for Home module

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.sidebar.title("Menu lateral")
 modulo  =  st.sidebar.selectbox("Elija un Acción",["Home","Ejercicio 01","Ejercicio 02","Ejercicio 03","Ejercicio 04"])
 
 if modulo ==  "Home":
-           runpy.run_path("Home.py")
+           st.execfile("Home.py")
       
 elif modulo == "Ejercicio 01":
   st.write("Estas en el módulo de Ejercicio 01")
