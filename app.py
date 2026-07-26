83e3564e30df1bc152a0ff61b4e040614f4a2ee1
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 13:36:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ st.sidebar.title("Menu lateral")
 modulo  =  st.sidebar.selectbox("Elija un Acción",["Home","Ejercicio 01","Ejercicio 02","Ejercicio 03","Ejercicio 04"])
 
 if modulo ==  "Home":
-      st.title("Trabajo Práctico Modulo 01")
-      
+           
       col_img1, col_img2 = st.columns(2) 
             
       with col_img1:
@@ -16,6 +15,7 @@ if modulo ==  "Home":
       with col_img2:
           st.image("DMC.png", width=250)
 
+      st.title("Trabajo Práctico Modulo 01")
       st.subheader("Oscar David Peñaherrera Cordova")
       st.write("Nombre del Modulo: Fundamentos de Phyton")
       st.write("Año:2026")
