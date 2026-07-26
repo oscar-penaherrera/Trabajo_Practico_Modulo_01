ccf0ce296e49d39e7f88423da77e98955244a216
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 13:58:56 -0500

    Import runpy and execute HOME.py in Home module

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import numpy as np
+import runpy
 
 st.sidebar.title("Menu lateral")
 
 modulo  =  st.sidebar.selectbox("Elija un Acción",["Home","Ejercicio 01","Ejercicio 02","Ejercicio 03","Ejercicio 04"])
 
 if modulo ==  "Home":
-           
+           runpy.run_path("HOME.py")
       
 elif modulo == "Ejercicio 01":
   st.write("Estas en el módulo de Ejercicio 01")
