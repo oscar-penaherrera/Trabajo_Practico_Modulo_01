4cdc27a1faaae6f1d255af2ea7939a0cd9dc2c6c
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:26:24 -0500

    Refactor columns to show images with captions
    Updated column layout to display two images side by side with captions.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import numpy as np
 
-col_left, col_rest = st.columns([1, 3]) # Columna pequeña a la izquierda, el resto del espacio a la derecha
-with col_left:
-    st.image("Python_logo.png",)
+col_img1, col_img2 = st.columns(2) # Esto crea dos columnas de igual ancho
 
-col_rest_right, col_right = st.columns([3, 1]) # El espacio restante a la izquierda, columna pequeña a la derecha
-with col_right:
-    st.image("DMC.png")
+with col_img1:
+    st.image("Python_logo.png", caption="Python Logo (Columna 1)", width=150)
+
+with col_img2:
+    st.image("DMC.png", caption="DMC Logo (Columna 2)", width=150)
+
+st.write()
     
 st.title("Trabajo Práctico Modulo 01")
 st.subheader("Oscar David Penaherrera Cordova")
