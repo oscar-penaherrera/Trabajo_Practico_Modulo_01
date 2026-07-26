36b1324a060a4a49caf88efee28894d10969c44b
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:23:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 st.image("Python_logo.png")
 col_left, col_rest = st.columns([1, 3]) # Columna pequeña a la izquierda, el resto del espacio a la derecha
 with col_left:
-    st.image("Python_logo.png", caption="Python Logo (Izquierda)")
+    st.image("Python_logo.png",)
 
 st.image("DMC.png")
 st.title("Trabajo Práctico Modulo 01")
