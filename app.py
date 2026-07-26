8aae4a72e78ab98f462e0e0dde32fd08a46e9bb6
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 13:09:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import numpy as np
+                              
+col_img1, col_img2 = st.columns(2) 
+      
+with col_img1:
+    st.image("Python_logo.png", width=250)
+      
+with col_img2:
+    st.image("DMC.png", width=250)
+      
+st.title("Trabajo Práctico Modulo 01")
+st.subheader("Oscar David Peñaherrera Cordova")
+st.write("Nombre del Modulo: Fundamentos de Phyton")
+st.write("Año:2026")
+st.write("Breve descripción del proyecto: Trabajo aplicativo")
+st.write("Tecnologías aplicadas: Google Colab, Streamlit y Github")
 
-
-                                
-    col_img1, col_img2 = st.columns(2) 
-    
-    with col_img1:
-        st.image("Python_logo.png", width=250)
-    
-    with col_img2:
-        st.image("DMC.png", width=250)
-    
-    st.title("Trabajo Práctico Modulo 01")
-    st.subheader("Oscar David Peñaherrera Cordova")
-    st.write("Nombre del Modulo: Fundamentos de Phyton")
-    st.write("Año:2026")
-    st.write("Breve descripción del proyecto: Trabajo aplicativo")
-    st.write("Tecnologías aplicadas: Google Colab, Streamlit y Github")
 
 
    
