c65c52d9b011cdf701116d7a0bb0111c8bc63a60
oscar-penaherrera <oscar.penaherrera@gmail.com>
2026-07-26 12:30:08 -0500

    Add title and subheader to Streamlit app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 
-# Esto crea dos columnas de igual ancho
+st.title("Trabajo Práctico Modulo 01")
+
 col_img1, col_img2 = st.columns(2) 
 
 with col_img1:
@@ -12,7 +13,7 @@ with col_img2:
 
 st.write()
     
-st.title("Trabajo Práctico Modulo 01")
+
 st.subheader("Oscar David Penaherrera Cordova")
 
 
